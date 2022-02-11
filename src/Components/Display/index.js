56dd31171e2b278642c118aa66d2198987b24f34
craigmaru <craigmarubeats@gmail.com>
2022-02-11 11:29:54 +0000

    get request done

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,17 +1,32 @@
 import Input from "../Input";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Display() {
-  return (
-    <div>
-      <Input />
+   const search = "london";
 
-      <p>Test</p>
-      <p>Info 1</p>
-      <p>Info 2</p>
-      <p>Info 3</p>
-    </div>
-  );
+   const [data, setData] = useState([]);
+
+   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=06d06f283858dd96ea4dac608af07ae9&units=metric`;
+
+   useEffect(() => {
+      async function getData() {
+         let response = await fetch(URL);
+         let data = await response.json();
+         setData(data.weather);
+      }
+      getData();
+   }, [URL]);
+
+   return (
+      <div>
+         <Input />
+
+         <p>Test</p>
+         <p>Info 1</p>
+         <p>Info 2</p>
+         <p>Info 3</p>
+      </div>
+   );
 }
 
 export default Display;
