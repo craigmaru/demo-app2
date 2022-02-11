f3e7a14d2c15087b1f3a136877f89b30b11c2b94
AsmaDage <asmadage@gmail.com>
2022-02-11 12:33:10 +0000

    conditional error fixed

--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,20 +1,27 @@
 import Input from "../Input";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function Display() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+  const firstMount = useRef(false);
 
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=06d06f283858dd96ea4dac608af07ae9&units=metric`;
 
   useEffect(() => {
+    if (firstMount.current === false) {
+      firstMount.current = true;
+      return;
+    }
+    if (firstMount.current) {
+      getData();
+    }
     async function getData() {
       let response = await fetch(URL);
       let data = await response.json();
       setData(data);
-      // return data
+      return data;
     }
-    getData();
   }, [URL]);
 
   function getInput(input) {
@@ -26,7 +33,8 @@ function Display() {
   return (
     <div>
       <Input getInput={getInput} />
-      {data ? <p>{data.weather[0].description}</p> : <></>}
+      <p>{data.name}</p>
+      {data.weather && <p>{data.weather[0].description}</p>}
     </div>
   );
 }
