7e3d8925337b0d8ae333a1e4fcd8b714f1cccd7c
craigmaru <craigmarubeats@gmail.com>
2022-02-11 10:32:58 +0000

    first commit

---
+++ src/Components/Display/index.js
@@ -0,0 +1,12 @@
+function Display() {
+   return (
+      <div>
+         <p>Test</p>
+         <p>Info 1</p>
+         <p>Info 2</p>
+         <p>Info 3</p>
+      </div>
+   );
+}
+
+export default Display;
